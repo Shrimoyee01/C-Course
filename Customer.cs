34d664700ebe8c805c64f37dfe5cfe4f70c4a0a2
Shrimoyee01 <61573269+Shrimoyee01@users.noreply.github.com>
2023-02-20 20:33:33 -0800

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<OrderBase> Orders;      // store objects of type order in this list
+
+        public Customer()       // default and paramaterless constructor
+        {
+            Orders = new List<OrderBase>();     // initialise the order list
+        }
+        public Customer(int id) 
+            : this()
+        { this.Id = id; }
+
+        public Customer(int id, string name) 
+            : this(id)     // Constructor that takes an id parameter
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ Point.cs
@@ -0,0 +1,28 @@
+﻿namespace Program
+{
+    public class Point
+    {
+        public int X; 
+        public int Y;
+
+        public Point (int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(Point newLocatioon)
+        {
+            if (newLocatioon == null)
+            {
+                throw new ArgumentNullException(nameof(newLocatioon));      // Defensive Programming --> if this is null, then the next line is never reached
+            }
+            Move(newLocatioon.X,newLocatioon.Y);    
+        }
+    }
+}
